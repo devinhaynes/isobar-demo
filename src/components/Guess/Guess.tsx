e11e182eaacb3eb830763bb91b520551191980b9
Devin Haynes <dhaynes916@gmail.com>
2023-02-04 20:30:04 -0500

    Minor adjustments

--- src/components/Guess/Guess.tsx
+++ src/components/Guess/Guess.tsx
@@ -2,10 +2,11 @@ import { FC, SyntheticEvent, useState } from "react";
 import "./Guess.scss";
 
 export const Guess: FC = () => {
-  const [results, setResults] = useState<string>();
-  const [answer, setAnswer] = useState<number>(
-    parseInt(Math.random() * (100 - 1) + 1)
-  );
+  const createAnswer = () => Math.floor(Math.random() * (100 - 1) + 1);
+
+  const [results, setResults] = useState<"Success" | "Fail" | null>();
+  const [resultsMessage, setResultsMessage] = useState<string>();
+  const [answer, setAnswer] = useState<number>(createAnswer());
   const [numGuesses, setNumGuesses] = useState<number>(1);
   const [inProgress, setInProgress] = useState<boolean>(true);
 
@@ -19,37 +20,48 @@ export const Guess: FC = () => {
     const value = parseInt(target.guess.value);
 
     if (value === answer) {
-      setResults(`You won in ${numGuesses} guesses!`);
+      setResults("Success");
+      setResultsMessage(`You won in ${numGuesses} guesses!`);
       setInProgress(false);
     }
 
-    if (value > answer) setResults("Too high");
-    if (value < answer) setResults("Too Low");
+    if (value > answer) {
+      setResults("Fail");
+      setResultsMessage("Too high");
+    }
+    if (value < answer) {
+      setResults("Fail");
+      setResultsMessage("Too low");
+    }
   };
 
   const playAgain = () => {
-    setAnswer(parseInt(Math.random() * (100 - 1) + 1));
+    setAnswer(createAnswer());
     setNumGuesses(1);
-    setResults("");
+    setResults(null);
+    setResultsMessage("");
     setInProgress(true);
   };
   return (
     <div className="Guess">
-      {inProgress ? (
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="Guess a Number between 1 and 100">
-            Guess a Number between 1 and 100
-          </label>
-          <input type="number" name="guess" />
-          <button className="btn-lg">Submit</button>
-        </form>
-      ) : (
-        <button className="btn-lg" onClick={playAgain}>
-          Play Again
-        </button>
-      )}
-
-      <div className="results">{results}</div>
+      <div data-status={results} className="results">
+        {resultsMessage}
+      </div>
+      <div className="Guess__wrapper">
+        {inProgress ? (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="Guess a Number between 1 and 100">
+              Guess a Number between 1 and 100
+            </label>
+            <input type="number" name="guess" />
+            <button className="btn-lg">Submit</button>
+          </form>
+        ) : (
+          <button className="btn-lg" onClick={playAgain}>
+            Play Again
+          </button>
+        )}
+      </div>
     </div>
   );
 };
